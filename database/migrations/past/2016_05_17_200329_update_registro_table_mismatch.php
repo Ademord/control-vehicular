7e6c2b349fe81ce3bf188385349827e8aef9350e
Ademord <druidelf3@gmail.com>
2016-11-21 18:17:52 +0100

    update migration for import

--- database/migrations/past/2016_05_17_200329_update_registro_table_mismatch.php
+++ database/migrations/past/2016_05_17_200329_update_registro_table_mismatch.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateRegistroTable extends Migration
+class UpdateRegistroTableMismatch extends Migration
 {
     /**
      * Run the migrations.
