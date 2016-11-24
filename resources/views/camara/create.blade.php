dcffaa1c8fb0cd46ec4744f02036bff11b0d3219
Ademord <druidelf3@gmail.com>
2016-11-25 00:21:29 +0100

    Add correct views for each entity

--- resources/views/camara/create.blade.php
+++ resources/views/camara/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.default')
 @section('content')
 	<div class="page-header">
-    <h1>Nuevo Cliente</h1>
+    <h1>Nueva Camara</h1>
 	</div>
   
   @include('camara.partials._session')
 
   {!! Form::open(array('route' => 'camara.store')) !!}
-    @include('camara.partials._form')
+    @include('camara.partials._form2')
   {!! Form::close() !!}
 @stop
